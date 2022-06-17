bc15d733a3e0647c5ed31b8af31127fbf5762dfe
tuNovio <alfgomachi@gmail.com>
2022-06-16 19:18:24 -0500

    probando el panel tipo stack

---
+++ practica-css/src/panel_stack.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class panel_stack extends Application{
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+
+}
